0961fa6dd9013a68b2ab153e409668891fcdc1fa
obiomaokorowu <austinobi983@gmail.com>
2023-02-12 12:35:41 +0100

    initial commit

---
+++ jenkins_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+sudo apt update
+sudo apt install openjdk-11-jdk -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 62A9756BFD780C377CF24BA8FCEF32E745F2C3D5
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install ca-certificates
+sudo apt update
+sudo apt install git -y
+sudo apt install maven -y
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-key fingerprint 0EBFCD88
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
+sudo usermod -aG docker $USER
