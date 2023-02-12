65d076c7885f101392d8b7d104808135180a4fa3
obiomaokorowu <austinobi983@gmail.com>
2023-02-12 14:29:18 +0100

    docker-install

--- main.tf
+++ main.tf
@@ -127,3 +127,15 @@ resource "aws_instance" "ec2_instance" {
   }
 }
 
+resource "aws_instance" "ec2_instance1" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = "dec-key"
+  user_data            = "${file("docker-install.sh")}"
+
+  tags = {
+    Name = "Docker-server"
+  }
+}
