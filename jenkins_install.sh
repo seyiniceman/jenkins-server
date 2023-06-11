ed8993267a791e5cfb6577ec80ae9c0d0e5e23b2
obiomaokorowu <109354887+obiomaokorowu@users.noreply.github.com>
2023-06-11 11:50:03 +0100

    Update jenkins_install.sh

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -31,7 +31,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-key fingerprint 0EBFCD88
 sudo apt-get update 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install -y kubectl=1.21.0-00
 sudo apt install awscli -y
 wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz
 tar -zxvf helm-v3.2.4-linux-amd64.tar.gz
